1668763e5627d7e0d89e1537509d234488ef3f82
Dev Raikwar <68481001+devraikwar@users.noreply.github.com>
2020-10-28 12:07:03 -0700

    Create main.java

---
+++ DAY5/Assignment/main.java
@@ -0,0 +1,12 @@
+package com.Day5.Assignment;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Game game = new Game();
+        game.initGame();
+        game.play();
+    }
+
+}
